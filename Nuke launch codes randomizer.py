cee1660dfc51ffbde49de296660b3a63bf05300a
Darwin Sebastion Clark <darwin.s.clark@gmail.com>
2016-03-18 11:51:29 -0700

    Started reading capabilities and and Added a find function

--- Nuke launch codes randomizer.py
+++ Nuke launch codes randomizer.py
@@ -1,23 +1,36 @@
 
 import random
+import time
 from pygame.locals import *
 
-chars = 9#random.randrange(0,9)
 
 
-possibilitiesN = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+possibilitiesN = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+#'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+finding = 'YH53GXMHQQTZ'
 
+foo = open(code2Find)
+
+ 
+launch = []
 while True:
+    chars = random.randrange(0,15)
     for i in range(0,chars):
         code = []
-        letter = random.randrange(0,50)
+        letter = random.randrange(0,36)
         charecter = possibilitiesN[letter]
         code.append(charecter)
 
         for i in code:
-            print(i,end = "")
+            launch.append(i)
+            print(i, end = "")
+            checking = ''.join(str(e) for e in launch)
+            if checking == finding:#Ending the program
+                print('I found it!, it is' + checking)
+                exit
+    print('') 
 
-    print('')
+
 
 
 
