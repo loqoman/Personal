a6cfc2b1587fa13c902d25832aa97fb4642fa994
Darwin Sebastion Clark <darwin.s.clark@gmail.com>
2016-03-22 11:52:24 -0700

    Bug fixing

--- Nuke launch codes randomizer.py
+++ Nuke launch codes randomizer.py
@@ -6,30 +6,28 @@ from pygame.locals import *
 
 
 possibilitiesN = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-#'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-finding = 'YH53GXMHQQTZ'
 
-foo = open(code2Find)
+foo = open('Code.txt')
 
- 
+finding = foo.readline()
+code = []
+checking = ''
 launch = []
 while True:
     chars = random.randrange(0,15)
-    for i in range(0,chars):
-        code = []
+    for i in range(1,chars):
         letter = random.randrange(0,36)
         charecter = possibilitiesN[letter]
         code.append(charecter)
-
-        for i in code:
-            launch.append(i)
-            print(i, end = "")
-            checking = ''.join(str(e) for e in launch)
-            if checking == finding:#Ending the program
-                print('I found it!, it is' + checking)
-                exit
-    print('') 
-
+        print(code)
+        checking = checking.join(code)
+        #checking = 'M\n'
+        print (checking)
+        if checking == finding:#Ending the program
+            print('I found it!, it is ' + checking)
+            exit
+    #print('')
+    code = []
 
 
 
