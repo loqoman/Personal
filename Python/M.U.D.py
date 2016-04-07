b5e6a5f7e2a2f027dffe0b2d03bd1d0162511a37
loqoman <darwin.s.clark@gmail.com>
2016-04-07 07:27:53 -0700

    Started debugging The Player and NPC classes

--- Python/M.U.D.py
+++ Python/M.U.D.py
@@ -53,68 +53,83 @@ pygame.draw.rect(windowSurface,BACKGROUND,background_rect,0)
 '''
 #--Start of program --#
 class Player(object):
+
+	players = []#List of of all the players
 	def __init__ (self, name, gender):
-		self.name = name
-		self.gender = gender
+		self.name = str(name)
+		self.gender = bool(gender)
+		self.skills = []
+		self.S_level = []		
+		print(self.name)
+		#str(name) + "_Skills" = Player_skills
+		
 	def say(self, words):
 		print(words)
+	def add_skill(self,skill,level):
+		skills.append(str(skill))
+		level.append(str(level))
+		
 class NPC(object):   # the idea is to have the NPC something like:  <townie1 = NPC(Bob, man)>
-	def __init__(self, name = 'NPC' ,layerN1 = "NPC",trait = 'aggressive'):
+	def __init__(self, name = 'NPC' ,layerN1 = 	"NPC",trait = 'aggressive'):
 		self.name = layerN1
 		self.trait = trait
 		self.Layer_Name_1 = name
 		print(layerN1)
 
-	def say(self, words):
+	def say(self, words, wait):#Basic command to hav the NPC say something
 	
 		print(self.name + ": " + words)
-	def learn(self,player):
+		time.sleep(wait)
+
+	def learn(self,player):#This is kinda a neat function that would allow the player to bond with the NPC by leaning 
 
         	self.name = self.Layer_Name_1
-        
+        	
 	def ask(self, question):
-    
         	responce =  input(self.name + ': ' + question + '\n')
         	return(responce)
     
 class Item(object):
-	def __init__ (self, title = 0, weapon = 'gun' ,DMG_min = 0, DMG_max = 10):# No name on the gun because the gun itself should be a name as defined in the __init_#
+	def __init__ (self,name = "John Doe", title = 0, weapon = 'gun' ,DMG_min = 0, DMG_max = 10,append = True):# OK, I changed it to have a name because that way it can be referenced to in other aruguments
 		self.weapon = weapon
 		self.DMG_min = DMG_min
 		self.DMG_max = DMG_max
+''' So I dont acutally think that we need this class [FOR NOW]
 class skill(object):
 	def __init__(self):
-		skills = []
-	def add(skill,level):
-		skills.append(skill)
-		skills.append(level)
- 
+		skills = []#Each level of the skill corrosponds to the skill directly above that lvel
+		skills_level = []
+	def add(self,skill,level):
+		skills.append(skills)
+		skills_level.append(level)
+'''
 		
 
 
 
-plasma = Item(title = 'hevenly', weapon = 'gun', DMG_min = 3, DMG_max = 10)
+plasma = Item(name = "Plasma",title = 'hevenly', weapon = 'gun', DMG_min = 3, DMG_max = 10)
 
+darwin = Player(name = 'been Boode', gender = True)
 Narritor = NPC(name = 'narritor', layerN1 = 'narritor')
 IntroGuide = NPC(name = 'Sylva-sylver-spring' , layerN1 = "Space-worker")# still not shure what the difference between ' and " is
 
 os.system('clear')
 
 # -- Start of printing test --# 
-Narritor.say("You come to, feeling groggy and have to blink you eyes a few times to wake up")
+Narritor.say("You come to, feeling groggy and have to blink you eyes a few times to wake up", 0)
 time.sleep(3)
-Narritor.say("You see a huminiod figure waving what looks like a flat device over some shiny flat square")
+Narritor.say("You see a huminiod figure waving what looks like a flat device over some shiny flat square", 0)
 time.sleep(3)
-IntroGuide.say ("Hello?")
+IntroGuide.say ("Hello?",0)
 time.sleep(1)
 IntroGuide.learn(player = "null" )
 time.sleep(1)
 print(IntroGuide.ask('How old are you?'))
-skill.add('eating','2')
-skill.add('looking','1')
-skill.add('pooping','3')
-for int in skill:
-	print(int)
+darwin.add_skill(skill = 'electonics', level = '2')
+#darwin.skills.add('looking','1')
+#darwin.skills.add('pooping','3')
+for str in darwin.skills:
+	print(str)
 
 
 
