6811bb9d051b20f3781d8ac557422399cffda65f
loqoman <darwin.s.clark@gmail.com>
2016-04-09 07:54:51 -0700

    Started a bunch of C programming because HYPOTHETICALLY speaking I can write cool programs in unix Right now I am working through the turorial at:http://www.tutorialspoint.com/cprogramming

---
+++ C/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// function declaration
+long func();
+long foo;
+long l;
+//---//
+int main() {
+
+   // function call
+   foo = 123123123123123123123;
+   long i = func();
+   long l = sizeof(long)
+   printf("%ld %ld \n",i,i);\
+   printf("%ld", l)
+}
+
+// function definition
+long func() {
+   	   
+   return foo;
+}
+
